9ab1f894a3d8b9e450f5fedc0929eeb1e87e4d66
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-09-14 10:20:13 +0530

    api: added publications route

--- app.js
+++ app.js
@@ -126,6 +126,30 @@ app.get("/about", (req,res)=>{
     }
 });
 
+app.get("/publications", (req,res)=>{
+    if (docStatus === 1){
+        doc.loadInfo().then(() => {
+            const sheet = doc.sheetsByIndex[4];
+            sheet.getRows().then((result)=>{
+                let output = [];
+                for (let item in result){
+                    let inner_dict = {}
+                    const data = result[item];
+                    const headers = data._sheet.headerValues;
+                    const rawdata = data._rawData;
+                    for(let index in headers){
+                        inner_dict[headers[index]] = rawdata[index];
+                    }
+                    output.push(inner_dict);
+                }
+                return res.status(200).send(output);
+            });
+        });
+    }else{
+        return res.status(500).send({"error":"Sheet is not ready"});
+    }
+});
+
 app.listen(process.env.PORT, () =>{
     console.log(`PES Innovation Lab API running on port ${process.env.PORT}!!`);
 });
